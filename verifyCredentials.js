b000eaa4680e4ed89fd02156c1b5ae194de1f899
slavikok2 <slavikok2@gmail.com>
2017-10-30 18:12:29 +0200

    asd

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,7 +1,6 @@
 'use strict';
 const request = require('request-promise');
 const co = require('co');
-const ut = require('./lib/helpers/http-utils');
 
 module.exports = function verifyCredentials(credentials, cb) {
     co(function*() {
